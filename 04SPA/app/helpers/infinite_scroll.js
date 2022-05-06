42c3e76d7c4d62549bbb963b120c2a59e9040681
matiscava <matiscava@hotmail.com>
2022-05-06 10:56:11 -0300

    Terminamos con la unidad de SPA

---
+++ 04SPA/app/helpers/infinite_scroll.js
@@ -0,0 +1,48 @@
+import { PostCard } from "../components/PostCard.js";
+import { SearchCard } from "../components/SearchCard.js";
+import { ajax } from "./ajax.js";
+import api from "./wp_api.js";
+
+export async function InfiniteScroll(){
+  const d = document,
+    w = window;
+
+  let query = localStorage.getItem('wpSearch'),
+    apiURL,
+    Component; //HOC: High Order Component
+
+  w.addEventListener('scroll', async (e) => {
+    let { scrollTop , clientHeight , scrollHeight } = d.documentElement,
+      { hash } = w.location;
+    
+    if(scrollTop + clientHeight >= scrollHeight){
+      api.page ++;
+
+      if( !hash || hash === '#/'){
+        apiURL = `${api.POSTS}&page=${api.page}`
+        Component = PostCard;
+      } else if( hash.includes('#/search')){
+        apiURL = `${api.SEARCH}${query}&page=${api.page}`
+        Component = SearchCard;
+      } else return false;
+      
+      d.querySelector('.loader').classList.display = 'block'
+
+
+      await ajax({
+          url: apiURL,
+          cbSuccess: (posts) => {
+            let html = ""
+            posts.forEach((post) => html += Component(post))
+            
+          d.querySelector('.loader').classList.display = 'none'
+
+            d.getElementById('main').insertAdjacentHTML('beforeend',html)
+          
+          }
+        }
+      )
+    }
+
+  })
+} 
