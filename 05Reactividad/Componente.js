8daeb29341ff646740b41cabffc55efcf1706f7c
matiscava <matiscava@hotmail.com>
2022-05-06 13:48:53 -0300

    Terminé con el curso de JS

---
+++ 05Reactividad/Componente.js
@@ -0,0 +1,38 @@
+const Component = (function(){
+  //creamos el Constructor del componente
+  const Constructor = function(options) {
+    this.el = options.el;
+    this.data = options.data;
+    this.template = options.template;
+  }
+
+  //Agregamos los métodos al prototipo del constructor del componente
+
+  //Render UI
+  Constructor.prototype.render = function() {
+    const $el = document.querySelector(this.el);
+
+      if(!$el) return;
+
+      $el.innerHTML = this.template(this.data);
+
+      console.log(this.data);
+  };
+
+  //Actualizar el State de forma reactiva
+  Constructor.prototype.setState = function(obj) {
+    for (const key in obj) {
+      if (this.data.hasOwnProperty(key)) {
+        this.data[key] = obj[key];
+      }
+    }
+    this.render();
+  };
+  
+  //Obtgenemos una copia inmutable del State
+  Constructor.prototype.getState = function() {
+    return JSON.parse(JSON.stringify(this.data))
+  };
+
+  return Constructor;
+})(); 
